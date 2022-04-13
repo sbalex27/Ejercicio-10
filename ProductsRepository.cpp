79551c580c0f7a90d462d6079c31f6f79215ee76
Alexander Batres <alex2000.sabe@gmail.com>
2022-04-13 13:01:42 -0600

    Functional main menu

--- ProductsRepository.cpp
+++ ProductsRepository.cpp
@@ -2,16 +2,16 @@
 
 ProductsRepository::ProductsRepository(std::string file_provider, std::string temp_provider)
 {
-	this->file_provider = file_provider.c_str();
-	this->temp_provider = temp_provider.c_str();
+	this->file_provider = file_provider;
+	this->temp_provider = temp_provider;
 }
 
 std::vector<ProductEntity> ProductsRepository::index()
 {
 	return {
-		ProductEntity("iPhone X", 3000.0f, 30),
-		ProductEntity("iPhone XS", 3500.0f, 20),
-		ProductEntity("iPhone XR", 3250.0f, 10),
+		ProductEntity("iPhone", 3000.0f, 30),
+		ProductEntity("Samsung", 3500.0f, 20),
+		ProductEntity("Xaomi", 3250.0f, 10),
 	};
 }
 
--- Source.cpp
+++ Source.cpp
@@ -1,8 +1,74 @@
 #include<iostream>
+#include"ProductsRepository.h"
+#include"ProductEntity.h"
 
 using namespace std;
 
 int main() {
-	
+	int option = 0;
+	ProductsRepository* repository = new ProductsRepository("data.txt", "temporal.txt");
+
+	do
+	{
+		cout << "1. Lista de productos" << endl;
+		cout << "2. Buscar un producto" << endl;
+		cout << "3. Crear un producto" << endl;
+		cout << "4. Modificar un producto" << endl;
+		cout << "5. Borrar un producto" << endl;
+		cin >> option;
+
+		switch (option)
+		{
+		case 0:
+			exit(0);
+			break;
+		case 1:
+		{
+			for (auto& i : repository->index()) i.print();
+			break;
+		}
+		case 2:
+		{
+			string to_find;
+			cout << "Buscar a: "; cin >> to_find;
+			repository->show(to_find).print();
+		}
+		case 3:
+		{
+			ProductEntity* entity = new ProductEntity();
+			entity->request();
+			repository->store(entity);
+			break;
+		}
+		case 4:
+		{
+			string to_find;
+			cout << "Modificar a: "; cin >> to_find;
+			ProductEntity entity = repository->show(to_find);
+
+			cout << "Antiguos Valores: " << endl;
+			entity.print();
+
+			cout << "Nuevos Valores: " << endl;
+			entity.request();
+			break;
+		}
+		case 5:
+		{
+			string to_delete;
+			cout << "Eliminar a: "; cin >> to_delete;
+			ProductEntity deleted = repository->show(to_delete);
+			repository->destroy(to_delete);
+
+			cout << "Se elimino a: " << endl;
+			deleted.print();
+			break;
+		}
+		default:
+			cout << "Opcion invalida." << endl;
+			break;
+		}
+	} while (option != 0);
+
 	return 0;
 }
